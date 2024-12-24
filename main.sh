8323d2ec4eb4fc0c454c009c8b2fcda9f7c6bd6e
SomeoneAlt-86 <76504556+SomeoneAlt-86@users.noreply.github.com>
2024-12-24 15:02:37 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,10 +1,5 @@
 #/bin/bash
-echo -n "Processing"
-for i in {1..4}; do
-    echo -n "."   # Print a dot without a newline
-    sleep 1       # Wait 1 second
-done
-echo " Done!"
+echo 'Scanning all available file types ...'
 SECONDS=0
 /System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -dump | grep applescript | grep -o '\.\w\+'
 echo 'Recorded all file types!' 
