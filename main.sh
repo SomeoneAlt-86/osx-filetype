3eed86eb743a166933cb8e343fae750f544d0e78
SomeoneAlt-86 <76504556+SomeoneAlt-86@users.noreply.github.com>
2024-12-24 15:05:29 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -3,4 +3,4 @@ echo 'Scanning all available file types ...'
 SECONDS=0
 /System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -dump | grep applescript | grep -o '\.\w\+'
 echo 'Recorded all file types!' 
-echo "finished execution in $SECONDS"
+echo "finished execution in $SECONDS seconds."
