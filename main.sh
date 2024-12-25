f3b9770524dcb019da078f879e6f329702aedc07
SomeoneAlt-86 <76504556+SomeoneAlt-86@users.noreply.github.com>
2024-12-25 07:49:57 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,6 +1,6 @@
-#/bin/bash
+#!/bin/bash
 echo 'Scanning all available file types ...'
 SECONDS=0
-/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -dump | grep -o '\.\w\+'
-echo 'Recorded all file types!' 
+/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -dump | grep -o '\.[a-zA-Z0-9]\{1,5\}' | sort -u
+echo 'Recorded all file types!'
 echo "finished execution in $SECONDS seconds."
